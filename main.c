cdf09557f9320e1ffff9b49b253d157e827242d0
orumin <dev@orum.in>
2016-11-06 15:37:13 +0900

    uefi memmap test

--- main.c
+++ main.c
@@ -1,51 +1,54 @@
 #include<efi.h>
 #include<efilib.h>
 
+# define MEMMAP_SIZE (1024*1024)
+UINT8 memmap[MEMMAP_SIZE * sizeof(EFI_MEMORY_DESCRIPTOR)];
+
+// entry point
 EFI_STATUS
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
-    EFI_DEVICE_PATH *Path;
-    EFI_LOADED_IMAGE *LoadedImageParent;
-    EFI_LOADED_IMAGE *LoadedImage;
-    EFI_HANDLE Image;
-    CHAR16 *Options = L"root=/dev/sda2 rootfstype=btrfs rw quiet splash";
-    EFI_STATUS Status=EFI_SUCCESS;
+    EFI_STATUS status = EFI_SUCCESS;
+
+    UINTN memmapSize = MEMMAP_SIZE;
+    UINTN mapKey, descriptorSize;
+    UINT32 descriptorVersion;
+
+    EFI_MEMORY_DESCRIPTOR *memoryDescriptor;
+
+    EFI_INPUT_KEY key;
 
     InitializeLib(ImageHandle, SystemTable);
     Print(L"Hello, EFI!\n");
 
-    Status = uefi_call_wrapper(BS->OpenProtocol, 6, ImageHandle, &LoadedImageProtocol, &LoadedImageParent, ImageHandle, NULL, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
-    if (EFI_ERROR(Status)) {
-        Print(L"Could not get LoadedImageProtocol handler %r\n", Status);
-        return Status;
+// get firmwware memory map
+    status = uefi_call_wrapper(BS->GetMemoryMap, 5, &memmapSize, (EFI_MEMORY_DESCRIPTOR*)memmap, &mapKey, &descriptorSize, &descriptorVersion );
+    if (EFI_ERROR(status)) {
+        Print(L"Could not get memory map %r\n", status);
+        for ( ;; )
+            ;
     }
 
-    Path = FileDevicePath(LoadedImageParent->DeviceHandle, L"\\vmlinuz");
-    if (Path == NULL) {
-        Print(L"Could not get device path.");
-        return EFI_INVALID_PARAMETER;
-    }
-    
-    Status = uefi_call_wrapper(BS->LoadImage, 6, FALSE, ImageHandle, Path, NULL, 0, &Image);
-    if (EFI_ERROR(Status)) {
-        Print(L"Could not load %r", Status);
-        FreePool(Path);
-        return Status;
-    }
+    Print(L"memmapSize = 0x%08x; descriptorSize = %d; sizeof = %d\n\n", memmapSize, descriptorSize, sizeof( EFI_MEMORY_DESCRIPTOR ));
+    memoryDescriptor = (EFI_MEMORY_DESCRIPTOR*)memmap;
 
-    Status = uefi_call_wrapper(BS->OpenProtocol, 6, Image, &LoadedImageProtocol, &LoadedImage, ImageHandle, NULL, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
-    if (EFI_ERROR(Status)) {
-        Print(L"Could not get LoadedImageProtocol handler %r\n", Status);
-        uefi_call_wrapper(BS->UnloadImage, 1, Image);
-        FreePool(Path);
-        return Status;
-    }
-    LoadedImage->LoadOptions = Options;
-    LoadedImage->LoadOptionsSize = (StrLen(LoadedImage->LoadOptions)+1) * sizeof(CHAR16);
+    do {
+        status = uefi_call_wrapper(ST->ConIn->ReadKeyStroke, 2, ST->ConIn, &key);
+    } while (status == EFI_NOT_READY);
 
-    Status = uefi_call_wrapper(BS->StartImage, 3, Image, NULL, NULL);
-    uefi_call_wrapper(BS->UnloadImage, 1, Image);
-    FreePool(Path);
+//    for ( int i=0; i<memmapSize; i+=3 ) {
+//        for ( int j=0; j<3; ++j ) {
+//            Print( L"Memory Type: %d\r\nPhysical Start: %08x\r\nVirtual Start: %08x\r\nNumber of Pages: %d\r\nAttribute: %08x\r\n\r\n",
+//                memoryDescriptor->Type, memoryDescriptor->PhysicalStart, memoryDescriptor->VirtualStart, memoryDescriptor->NumberOfPages, memoryDescriptor->Attribute );
+//            memoryDescriptor = (EFI_MEMORY_DESCRIPTOR*)((UINT8*)memoryDescriptor + descriptorSize);
+//        }
+//    }
+
+    do {
+        status = uefi_call_wrapper(ST->ConIn->ReadKeyStroke, 2, ST->ConIn, &key);
+    } while (status == EFI_NOT_READY);
 
     return EFI_SUCCESS;
+
 }
+
